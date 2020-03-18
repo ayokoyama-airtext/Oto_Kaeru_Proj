81fb30ec2f7db676a5404b169d645e95df7293ae
yoshiki-shimada <yothiki321@gmail.com>
2020-03-18 18:59:38 +0900

    20200318_大丈夫なはず

---
+++ Oto_Kaeru_Proj/Source/Hoomy_2D/Shimada/Otosama.cpp
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Otosama.h"
+#include "PaperFlipbookComponent.h"
+#include "Components/TextRenderComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+
+AOtosama::AOtosama()
+{
+	// もし移動させるときヨーが移動しないように
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = true;
+	bUseControllerRotationRoll = false;
+
+	// コリジョンのサイズを設定
+	GetCapsuleComponent()->SetCapsuleHalfHeight(120.0f);
+	GetCapsuleComponent()->SetCapsuleRadius(120.0f);
+
+	// キャラクタの自動回転を防止
+	GetCharacterMovement()->bOrientRotationToMovement = false;
+
+	// 設定
+	GetCharacterMovement()->GravityScale = 3.0f;
+
+	// キャラの動きをXZにロック
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->SetPlaneConstraintNormal(FVector(0.0f, -1.0f, 0.0f));
+
+	// Capsuleのローテーションを防止
+	GetCharacterMovement()->bUseFlatBaseForFloorChecks = true;
+
+	// Spriteコンポーネントでレプリケーションを有効にして、ネットワーク化されたときにアニメーションが表示させる
+	GetSprite()->SetIsReplicated(true);
+	bReplicates = true;
+
+	InWaterFlag = false;
+}
+
+void AOtosama::UpdateAnimation()
+{
+	const FVector PlayerVelocity = GetVelocity();
+	const float PlayerSpeedSqr = PlayerVelocity.SizeSquared();
+
+	// 状態をNotSwimにする
+	UPaperFlipbook* DesiredAnimation = (PlayerSpeedSqr > 0.0f) ? NotSwimmingAnimation : SwimmingAnimation;
+	if (GetSprite()->GetFlipbook() != DesiredAnimation)
+	{
+		GetSprite()->SetFlipbook(DesiredAnimation);
+	}
+}
+
+/*
+* @brief 毎フレーム処理
+*/
+void AOtosama::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	UpdateCharacter();
+}
+
+void AOtosama::InWater()
+{
+	GetSprite()->SetFlipbook(SwimmingAnimation);
+
+	if (InWaterFlag)
+		InWaterFlag = true;
+}
+
+void AOtosama::UpdateCharacter()
+{
+	//! アニメーション切替
+	UpdateAnimation();
+
+	// ほかの処理を書くなら
+}
---
+++ Oto_Kaeru_Proj/Source/Hoomy_2D/Shimada/Otosama.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "Otosama.generated.h"
+
+class UTextRenderComponent;
+
+/**
+ * @brief MainChara(Otonosama)
+ */
+UCLASS(Config = Game)
+class HOOMY_2D_API AOtosama : public APaperCharacter
+{
+	GENERATED_BODY()
+
+		UTextRenderComponent* TextComponent;
+	virtual void Tick(float DeltaSeconds) override;
+
+protected:
+	// 水中にいないときのアニメーション
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+		class UPaperFlipbook* NotSwimmingAnimation;
+
+	// 水中にいるときのアニメーション
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+		class UPaperFlipbook* SwimmingAnimation;
+
+	// アニメーション切替用
+	void UpdateAnimation();
+
+	// 水ブロックに入った時
+	void InWater();
+
+	void UpdateCharacter();
+
+public:
+	AOtosama();
+
+	bool InWaterFlag;
+};
